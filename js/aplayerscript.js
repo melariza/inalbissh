b05701e8a1208a2b6a9e480766e6662aabed312c
melariza <38114902+melariza@users.noreply.github.com>
2019-07-13 17:40:56 +0100

    aplayer

---
+++ js/aplayerscript.js
@@ -0,0 +1,21 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    listFolded: false,
+    listMaxHeight: 90,
+    audio: [
+    	{
+	        name: 'Mud Blood',
+	        artist: 'Loïc Nottet',
+	        url: 'https://ia800606.us.archive.org/26/items/01BadLiar/01%20Bad%20Liar.mp3',
+	        cover: 'http://p2.music.126.net/o8GErRrQ5v-NJuTt-emNRQ==/17707634765790510.jpg',
+            theme: '#46718b'
+    	},
+        {
+            name: 'name2',
+            artist: 'artist2',
+            url: 'https://ia800606.us.archive.org/26/items/01BadLiar/01%20Bad%20Liar.mp3',
+            cover: 'http://p2.music.126.net/o8GErRrQ5v-NJuTt-emNRQ==/17707634765790510.jpg',
+            theme: '#46718b'
+        }    	
+    ]
+});
